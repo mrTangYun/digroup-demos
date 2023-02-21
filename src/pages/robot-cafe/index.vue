2ed9496f0a2b0f5ecde6769828f6e50461eb3e1f
tangyun <tangyun@51qub.com>
2023-02-21 09:21:07 +0800

    全屏, 按钮只动一次

--- src/pages/robot-cafe/index.vue
+++ src/pages/robot-cafe/index.vue
@@ -1,38 +1,45 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 // import imgUrl from './images/底版.jpg'
 const imgUrl = new URL('./images/底版.jpg', import.meta.url).href
 const aniuimgUrl = new URL('./images/按钮.png', import.meta.url).href
 
 const btn = ref(null);
 const isStart = ref(false);
+const isFullPage = ref(false);
+const fullPage = ref(null);
 
 
 defineProps({
   msg: String,
 })
 
+onMounted(() => {
+  btn.value.addEventListener('animationend', (event) => {
+    isStart.value = false;
+    console.log('animationend');
+  });
+})
+
 function toggleFullScreen() {
-    console.log(isStart.value);
+  if (!isFullPage.value) {
+    if (!document.fullscreenElement) {
+      // If the document is not in full screen mode
+      // make the video full screen
+      fullPage.value.requestFullscreen();
+      isFullPage.value = true;
+    }
+  }
   isStart.value = !isStart.value;
-  // if (!document.fullscreenElement) {
-  //   // If the document is not in full screen mode
-  //   // make the video full screen
-  //   btn.value.requestFullscreen();
-  // } else {
-  //   // Otherwise exit the full screen
-  //   if (document.exitFullscreen) {
-  //     document.exitFullscreen();
-  //   }
-  // }
+  
 }
 
 </script>
 
 <template>
-  <div class="fullPage">
+  <div class="fullPage" ref="fullPage">
     <img class="bg" :src="imgUrl" />
-    <img :class="['anniu', {start: isStart}]" :src="aniuimgUrl" @click="toggleFullScreen" />
+    <img :class="['anniu', {start: isStart}]" :src="aniuimgUrl" @click="toggleFullScreen" ref="btn" />
   </div>
 </template>
 
@@ -65,7 +72,7 @@ function toggleFullScreen() {
       animation-name: light;
       animation-duration: 1s;
       animation-timing-function: linear;
-      animation-iteration-count: infinite;
+      animation-iteration-count: 2;
       animation-direction: alternate;
       transform-origin: 80% 80%;
     }
