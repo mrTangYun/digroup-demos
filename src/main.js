87417d7674fcdaf1507f8b7ec013174b553ef6b4
tangyun <tangyun@51qub.com>
2023-02-21 16:44:54 +0800

    安装router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import Demos from './pages/robot-cafe/index.vue'
+import Demos from './pages/robot-cafe/index.vue';
+// import {router} from './routers'
 
-createApp(Demos).mount('#app')
+// console.log(router);
+
+const app = createApp(Demos);
+// app.use(router)
+app.mount('#app')
